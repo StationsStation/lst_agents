ffc13a4147ff74cb3267db35aafc803d42a36afb
8ball030 <8baller@station.codes>
2025-09-09 10:19:28 +0100

    refactor:moved-behaviour-class

--- packages/lstolas/skills/lst_skill/behaviours.py
+++ packages/lstolas/skills/lst_skill/behaviours.py
@@ -25,6 +25,7 @@ from typing import Any, cast
 
 from aea.skills.behaviours import FSMBehaviour
 
+from lst_agent.vendor.lstolas.skills.lst_skill.behaviours import CheckAnyWorkRound
 from packages.lstolas.skills.lst_skill.behaviours_classes.base_behaviour import (
     BaseState,
     LstabciappEvents,
@@ -154,35 +155,6 @@ class ClaimRewardTokensRound(BaseState):
         self.log.info("Claiming reward tokens...")
         self._is_done = True
         self._event = LstabciappEvents.DONE
-
-
-class CheckAnyWorkRound(BaseState):
-    """This class implements the behaviour of the state CheckAnyWorkRound."""
-
-    _state = LstabciappStates.CHECKANYWORKROUND
-
-    conditional_behaviours_to_events: list[tuple[LstabciappStates, LstabciappEvents]] = []
-
-    def setup(self) -> None:
-        """Setup the conditional behaviours."""
-        self.conditional_behaviours_to_events = [
-            (LstabciappStates.CLAIMBRIDGEDTOKENSROUND, LstabciappEvents.CLAIM_BRIDGED_TOKEN),
-            (LstabciappStates.TRIGGERL2TOL1BRIDGEROUND, LstabciappEvents.TRIGGER_L2_TO_L1),
-            (LstabciappStates.FINALIZEBRIDGEDTOKENSROUND, LstabciappEvents.FINALIZE_BRIDGED_TOKEN),
-        ]
-
-    def act(self) -> None:
-        """Perform the act."""
-        self.log.info("Checking for any work to be done...")
-        self._event = LstabciappEvents.NO_WORK
-        for behaviour, event in self.conditional_behaviours_to_events:
-            instance: BaseState = self.context.behaviours.main.get_state(behaviour.value)
-            self.log.info(f"Checking condition for {behaviour}...")
-            if instance.is_triggered():
-                self._event = event
-                self._is_done = True
-                return
-        self._is_done = True
 
 
 class LstabciappFsmBehaviour(FSMBehaviour):
