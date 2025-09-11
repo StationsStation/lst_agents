2626d5521deb60c0b3e9548eb78ffa9443ae7be9
8ball030 <8baller@station.codes>
2025-09-11 11:31:51 +0100

    feat:added-checkpoint-transaction

--- packages/lstolas/skills/lst_skill/behaviours_classes/checkpoint_round.py
+++ packages/lstolas/skills/lst_skill/behaviours_classes/checkpoint_round.py
@@ -16,16 +16,15 @@ class CheckpointRound(BaseState):
     """This class implements the behaviour of the state CheckpointRound."""
 
     _state = LstabciappStates.CHECKPOINTROUND
-    current_staking_proxy: Address | None = None
+    callable_staking_proxies: list[Address]
 
     def act(self) -> None:
         """Perform the act."""
         self.log.info("Creating checkpoint...")
-        if self.current_staking_proxy is None:
-            self.log.error("No staking proxy set for checkpointing.")
-        else:
+        while self.callable_staking_proxies:
+            current_staking_proxy = self.callable_staking_proxies.pop()
             self.tx_settler.build_and_settle_transaction(
-                contract_address=self.current_staking_proxy,
+                contract_address=current_staking_proxy,
                 function=self.strategy.lst_staking_token_locked.claim,
                 ledger_api=self.strategy.layer_1_api,
             )
@@ -34,7 +33,6 @@ class CheckpointRound(BaseState):
 
     def is_triggered(self) -> bool:
         """Check whether the behaviour is triggered."""
-        self.current_staking_proxy = None
         current_block_ts = int(self.strategy.layer_2_api.api.eth.get_block("latest").timestamp)  # type: ignore
         raw_events = self.strategy.lst_staking_manager_contract.get_staked_events(
             self.strategy.layer_2_api,
@@ -64,6 +62,5 @@ class CheckpointRound(BaseState):
 
             if current_block_ts - last_checkpoint > liveness_period:
                 self.log.info(f"Checkpoint needed for staking proxy {staking_proxy}.")
-                self.current_staking_proxy = staking_proxy
-                return True
-        return False
+                self.callable_staking_proxies.append(staking_proxy)
+        return len(self.callable_staking_proxies) > 0
